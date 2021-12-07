3c463e54a6c03dedda0645e7f03891e9e1914023
BYY <53412286+MGYBY@users.noreply.github.com>
2021-12-07 16:48:52 -0500

    Add files via upload

---
+++ rw/front_runner/readInit/init.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# count the line number of the water depth field
+lines=`wc -l initH2D | awk '{print $1}'`
+
+awk -v lines=$lines '
+BEGIN {
+  print lines " 0 0"
+} {
+  print $1 " " $2 " " $3;
+}' < initH2D | delaunay > initH.gts
+
+awk -v lines=$lines '
+BEGIN {
+  print lines " 0 0"
+} {
+  print $1 " " $2 " " $3;
+}' < initU2D | delaunay > initU.gts
---
+++ rw/front_runner/readInit/runCase.sh
@@ -0,0 +1,20 @@
+# python3 create_mesh.py
+python3 create_mesh_parallel.py
+
+# merge parameter file
+cat meshFile >> shock_adapt1.gfs
+
+# gauge location file (centreline)
+awk 'BEGIN{ for (x = 0.0; x <= 162.0; x += 162.0/24000.) print x, 0.75, 0.;}' > gaugeLoc
+
+# mapped initial input file
+python3 format_text.py
+sh init.sh
+
+# run Gerris in parallel
+# gerris2D -m -b 3 shock_adapt1.gfs > parallelsimulation.gfs
+# mpirun -np 3 gerris2D -m shock_adapt1.gfs
+
+mpirun -np 4 gerris2D -m shock_adapt1.gfs
+
+# gerris2D -m shock_adapt1.gfs
