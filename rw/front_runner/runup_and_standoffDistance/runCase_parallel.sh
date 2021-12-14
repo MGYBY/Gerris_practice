eef628b5eb8d39a7801036b68b546328e06ee572
BYY <53412286+MGYBY@users.noreply.github.com>
2021-12-14 14:15:44 -0500

    Add files via upload

---
+++ rw/front_runner/runup_and_standoffDistance/runCase_parallel.sh
@@ -0,0 +1,24 @@
+# python3 create_mesh.py
+python3 create_mesh_parallel.py
+
+# merge parameter file
+cat meshFile >> shock_adapt1.gfs
+echo "Mesh finished."
+
+# gauge location file (centreline)
+awk 'BEGIN{ for (x = 0.0; x <= 162.0; x += 162.0/24000.) print x, 1.500, 0.;}' > gaugeLoc
+
+awk 'BEGIN{ for (x = 158.99; x <= 159.99; x += 1.0/128.) print x, 1.500, 0.;}' > centerlineGaugeLoc
+
+echo "Slice gauges finished."
+
+# run Gerris in parallel
+# gerris2D -m -b 3 shock_adapt1.gfs > parallelsimulation.gfs
+echo "Beginning main simulation program ... ..."
+mpirun -np 5 gerris2D -m shock_adapt1.gfs
+
+# gerris2D -s 1 shock_adapt1.gfs > splitsimulation.gfs
+# gerris2D -b 3 splitsimulation.gfs > parallelsimulation.gfs
+# mpirun -np 3 gerris2D parallelsimulation.gfs
+
+# gerris2D -m shock_adapt1.gfs
