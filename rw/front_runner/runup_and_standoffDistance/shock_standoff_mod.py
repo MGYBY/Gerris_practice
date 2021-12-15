e000c771746f292a3654c718cd4b970ee9948956
BYY <53412286+MGYBY@users.noreply.github.com>
2021-12-14 22:32:47 -0500

    Add files via upload

---
+++ rw/front_runner/runup_and_standoffDistance/shock_standoff_mod.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import numpy as np
+# import time
+import sys
+# import re
+import fnmatch
+import os
+
+# normal_depth = 0.00798
+obs_loc = 40.0
+sim_time = sys.argv[1]
+# print('Simulation time is:{0}'.format(sim_time))
+# a two-column file
+# a = np.loadtxt("centerlineSlice", usecols = (1,4), skiprows=1, dtype=np.float32)
+for file in os.listdir('.'):
+    if fnmatch.fnmatch(file, 'centerlineSlice*'):
+        a = np.loadtxt(file, usecols = (1,4), skiprows=1, dtype=np.float32)
+
+val = a[:,1]
+loc = a[:,0]
+# max_depth = np.max(val)
+# min_depth = np.min(val)
+bow_shock_depth = (np.max(val)+np.min(val))/2.0
+bow_shock_diff = np.abs(val-bow_shock_depth)
+min_diff_loc = np.argmin(bow_shock_diff)
+bow_shock_standoff = obs_loc-loc[min_diff_loc]
+# print('Location of bow shock is:{0}'.format(bow_shock_loc))
+with open('bowShockWidth','a') as file:
+    file.write("{0} {1}\n".format(sim_time, bow_shock_standoff))
